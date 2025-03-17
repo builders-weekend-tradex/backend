2378ed67fb73f4e99d007ddb9de3db83dcc4678f
NabbeunNabi <williamedwardbrammer@gmail.com>
2025-03-17 20:54:47 +0900

    feat: implement cors

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.analysis.social import router as social_router
 from routes.analysis.tech import router as tech_router
 from routes.analysis.lexi import router as lexi_router
 
 app = FastAPI()
+
+origins = [
+    "https://www.trade-x.me/",
+    "https://trade-x.me/",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(social_router)
 app.include_router(tech_router)
