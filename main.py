12d2c4d08c5706618131a3452710501bdd6723c3
NabbeunNabi <williamedwardbrammer@gmail.com>
2025-03-24 23:39:32 +0900

    fix: cors header exception

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI, Request
+from typing import Union
 from routes.analysis.social import router as social_router
 from routes.analysis.tech import router as tech_router
 from routes.analysis.lexi import router as lexi_router
@@ -17,11 +21,49 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=False,
+    allow_credentials=True,
+    allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["*"],
-    allow_methods=["*"]
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.exception_handler(Exception)
+async def exception_handler(request: Request, exception: Union[Exception, RuntimeError]):
+    headers = {
+        'Access-Control-Allow-Origin': ', '.join(origins),
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': '*',
+    }
+    if isinstance(exception, EntityException):
+        response = JSONResponse(
+            jsonable_encoder(
+                {
+                    "code": exception.code,
+                    "message": exception.message,
+                    "exception": exception.exception
+                }
+            ),
+            headers=headers
+        )
+    else:
+        response = JSONResponse(
+            jsonable_encoder(
+                {
+                    "exception": str(exception),
+                    "code": 500,
+                }
+            ),
+            headers=headers
+        )
+    return response
 
 app.include_router(social_router)
 app.include_router(tech_router)
